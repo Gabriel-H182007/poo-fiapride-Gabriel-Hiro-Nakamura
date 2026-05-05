1c412df99499741fb91dcd741e1a3ba6ea160f8a
Gabriel Hiro Nakamura <rm562221@fiap.com.br>
2026-05-05 20:00:53 -0300

    feat: aula9 - cria interface com metodo abstrato

---
+++ src/br/com/fiapride/model/Celular.java
@@ -0,0 +1,26 @@
+package br.com.fiapride.model;
+
+public class Celular implements Recarregavel{
+	 	private String marca;
+	    private int nivelBateria; // 0-100
+	    
+	    public Celular(String marca) {
+	        this.marca = marca;
+	        this.nivelBateria = 0;
+	    }
+	    
+	    @Override
+	    public void recarregar(int percentual) {
+	        if (percentual < Recarregavel.CARGA_MINIMA || percentual > Recarregavel.CARGA_MAXIMA) {
+	            System.out.println("Erro: Percentual inválido!");
+	            return;
+	        }
+	        
+	        this.nivelBateria = percentual;
+	        System.out.println("Celular " + this.marca + " carregado para " + percentual + "%");
+	    }
+	    
+	    public int getNivelBateria() {
+	        return this.nivelBateria;
+	    }
+}
